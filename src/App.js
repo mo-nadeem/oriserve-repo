f4136eef0c7c090ede84b70e9b251ad37dfe8a91
root <root@LAPTOP-2UFRSDR1.localdomain>
2024-09-26 07:28:22 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React V1 Application</h1>
+        <h1>React ToDo Application</h1>
         <div>
           <input
             type="text"
